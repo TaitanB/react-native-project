856c36884d6753dacf104e696b865afcc86060db
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-04-21 18:18:39 +0300

    start

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello app!</Text>
+      <Text>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
