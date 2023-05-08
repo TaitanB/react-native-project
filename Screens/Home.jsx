7e67e4dae5f353b6b9eba424669364cf1eb288d9
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-05-08 15:15:27 +0300

    style

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -15,51 +15,47 @@ const Home = () => {
   const navigation = useNavigation();
 
   return (
-    <Tabs.Navigator
-      initialRouteName="PostsSceen"
-      screenOptions={({ route }) => ({
-        // tabBarIcon: ({ focused, color, size }) => {
-        //   let iconName;
-        //   if (route.name === "Profile") {
-        //     iconName = "person-outline";
-        //   } else if (route.name === "CreatePosts") {
-        //     iconName = "add";
-        //   } else if (route.name === "Posts") {
-        //     iconName = "md-grid-outline";
-        //   }
-        //   return <Ionicons name={iconName} size={size} color={color} />;
-        // },
-        tabBarItemStyle: {
-          borderRadius: 20,
-          height: 40,
-        },
-        tabBarHideOnKeyboard: true,
-        tabBarActiveTintColor: "#FF6C00",
-        tabBarInactiveTintColor: "#21212180",
-        tabBarStyle: [
-          {
-            height: 73,
-            paddingTop: 9,
-            paddingBottom: 22,
-            paddingHorizontal: 60,
-            borderTopWidth: 1,
-            borderTopColor: "#BDBDBD",
-            display:
-              route.name === "CreatePosts" ||
-              route.name === "Comments" ||
-              route.name === "Map"
-                ? "none"
-                : "flex",
+    <View style={styles.container}>
+      <Tabs.Navigator
+        initialRouteName="PostsSceen"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Profile") {
+              iconName = "user";
+            } else if (route.name === "CreatePosts") {
+              iconName = "plus";
+            } else if (route.name === "Posts") {
+              iconName = "grid";
+            }
+            return <Feather name={iconName} size={size} color={color} />;
           },
-        ],
-      })}
-    >
-      <Tabs.Screen
-        name="Posts"
-        component={PostsScreen}
-        options={{
+          tabBarItemStyle: {
+            borderRadius: 20,
+            marginHorizontal: 10,
+          },
+          tabBarHideOnKeyboard: true,
+          tabBarActiveTintColor: "#ffffff",
+          tabBarInactiveTintColor: "#21212180",
+          tabBarActiveBackgroundColor: "#FF6C00",
+          tabBarInactiveBackgroundColor: "#ffffff",
+          tabBarStyle: [
+            {
+              height: 71,
+              paddingTop: 9,
+              paddingBottom: 22,
+              paddingHorizontal: 60,
+              borderTopWidth: 1,
+              borderTopColor: "#BDBDBD",
+              display:
+                route.name === "CreatePosts" ||
+                route.name === "Comments" ||
+                route.name === "Map"
+                  ? "none"
+                  : "flex",
+            },
+          ],
           tabBarShowLabel: false,
-          headerTitle: "Posts",
           headerTitleStyle: {
             fontFamily: "Roboto-500",
             fontStyle: "normal",
@@ -77,47 +73,31 @@ const Home = () => {
             borderBottomWidth: 1,
             borderBottomColor: "#BDBDBD",
           },
-          headerRight: () => (
-            <TouchableOpacity
-              style={{
-                width: 24,
-                marginRight: 16,
-              }}
-              onPress={() => navigation.navigate("Login")}
-            >
-              <MaterialIcons name="logout" size={24} color={"#BDBDBD"} />
-            </TouchableOpacity>
-          ),
-          tabBarIcon: ({ focused, color, size }) => (
-            <View style={styles.iconContainer}>
-              <Ionicons name="grid-outline" size={size} color={color} />
-            </View>
-          ),
-        }}
-      />
-      {/* <Tabs.Screen
+        })}
+      >
+        <Tabs.Screen
+          name="Posts"
+          component={PostsScreen}
+          options={{
+            headerTitle: "Posts",
+            headerRight: () => (
+              <TouchableOpacity
+                style={{
+                  width: 24,
+                  marginRight: 16,
+                }}
+                onPress={() => navigation.navigate("Login")}
+              >
+                <MaterialIcons name="logout" size={24} color={"#BDBDBD"} />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        {/* <Tabs.Screen
         name="Map"
         component={MapScreen}
         options={{
-          tabBarShowLabel: false,
           headerTitle: "Map",
-          headerTitleStyle: {
-            fontFamily: "Roboto-500",
-            fontStyle: "normal",
-            fontWeight: "medium",
-            fontSize: 17,
-            lineHeight: 22,
-            textAlign: "center",
-            letterSpacing: -0.408,
-            color: "#212121",
-          },
-          headerTitleAlign: "center",
-          headerStatusBarHeight: 44,
-          headerStyle: {
-            height: 88,
-            borderBottomWidth: 1,
-            borderBottomColor: "#BDBDBD",
-          },
           headerLeft: () => (
             <TouchableOpacity
               style={{ width: 24, marginLeft: 16 }}
@@ -128,29 +108,11 @@ const Home = () => {
           ),
         }}
       /> */}
-      {/* <Tabs.Screen
+        {/* <Tabs.Screen
         name="Comments"
         component={CommentsScreen}
         options={{
           headerTitle: "Comments",
-          tabBarShowLabel: false,
-          headerTitleStyle: {
-            fontFamily: "Roboto-500",
-            fontStyle: "normal",
-            fontWeight: "medium",
-            fontSize: 17,
-            lineHeight: 22,
-            textAlign: "center",
-            letterSpacing: -0.408,
-            color: "#212121",
-          },
-          headerTitleAlign: "center",
-          headerStatusBarHeight: 44,
-          headerStyle: {
-            height: 88,
-            borderBottomWidth: 1,
-            borderBottomColor: "#BDBDBD",
-          },
           headerLeft: () => (
             <TouchableOpacity
               style={{ width: 24, marginLeft: 16 }}
@@ -161,71 +123,30 @@ const Home = () => {
           ),
         }}
       /> */}
-      <Tabs.Screen
-        name="CreatePosts"
-        component={CreatePostsScreen}
-        options={{
-          tabBarShowLabel: false,
-          headerTitle: "Create a post",
-          headerTitleStyle: {
-            fontFamily: "Roboto-500",
-            fontStyle: "normal",
-            fontWeight: "medium",
-            fontSize: 17,
-            lineHeight: 22,
-            textAlign: "center",
-            letterSpacing: -0.408,
-            color: "#212121",
-          },
-          headerTitleAlign: "center",
-          headerStatusBarHeight: 44,
-          headerStyle: {
-            height: 88,
-            borderBottomWidth: 1,
-            borderBottomColor: "#BDBDBD",
-          },
-          headerLeft: () => (
-            <TouchableOpacity
-              style={{ width: 24, marginLeft: 16 }}
-              onPress={() => navigation.navigate("Posts")}
-            >
-              <Ionicons name="arrow-back" size={24} color="#21212180" />
-            </TouchableOpacity>
-          ),
-          tabBarIcon: ({ focused, color, size }) => (
-            <View
-              style={{
-                ...styles.addIconContainer,
-                backgroundColor: focused ? "#ffffff" : "#FF6C00",
-              }}
-            >
-              <Ionicons
-                name="add-outline"
-                size={size}
-                color={color}
-                style={{
-                  ...styles.addIcon,
-                  color: focused ? "#21212180" : "#ffffff",
-                }}
-              />
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <View style={styles.iconContainer}>
-              <Feather name="user" size={size} color={color} />
-            </View>
-          ),
-        }}
-      />
-    </Tabs.Navigator>
+        <Tabs.Screen
+          name="CreatePosts"
+          component={CreatePostsScreen}
+          options={{
+            headerTitle: "Create a post",
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ width: 24, marginLeft: 16 }}
+                onPress={() => navigation.navigate("Posts")}
+              >
+                <Ionicons name="arrow-back" size={24} color="#21212180" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Tabs.Navigator>
+    </View>
   );
 };
 
@@ -234,22 +155,5 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  addIconContainer: {
-    width: 70,
-    height: 40,
-    borderRadius: 20,
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  iconContainer: {
-    width: 40,
-    height: 40,
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
